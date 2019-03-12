c10a9f184c53868429072460689db1818b1a7823
Evgeniy <7850039+own2pwn@users.noreply.github.com>
2019-03-12 19:59:42 +0300

    it's going harder

--- Quick/Quick/ViewController.swift
+++ Quick/Quick/ViewController.swift
@@ -27,9 +27,18 @@ let LayoutMethodHorizontally: Int = 8
 
 let LayoutMethodMarginTop: Int = 9
 
+let LayoutMethodAfter: Int = 10
+let LayoutMethodBefore: Int = 11
+
+let LayoutMethodSizeToFit: Int = 12
+let LayoutMethodSizeToFitWidth: Int = 13
+let LayoutMethodSizeToFitHeight: Int = 14
+
 public enum QuickLayoutMethodArgument {
     case constant(CGFloat)
     case containerSafeArea
+
+    case verticalAlign(UIView, CGFloat, VerticalAlign)
 }
 
 public extension QuickLayoutMethodArgument {
@@ -74,6 +83,13 @@ public enum QuickLayoutMethod {
     case horizontally
 
     case marginTop
+
+    case after
+    case before
+
+    case sizeToFit
+    case sizeToFitWidth
+    case sizeToFitHeight
 }
 
 extension QuickLayoutMethod: Hashable {}
@@ -104,6 +120,18 @@ extension QuickLayoutMethod {
 
         case LayoutMethodMarginTop:
             self = .marginTop
+
+        case LayoutMethodAfter:
+            self = .after
+        case LayoutMethodBefore:
+            self = .before
+
+        case LayoutMethodSizeToFit:
+            self = .sizeToFit
+        case LayoutMethodSizeToFitWidth:
+            self = .sizeToFitWidth
+        case LayoutMethodSizeToFitHeight:
+            self = .sizeToFitHeight
 
         default:
             return nil
@@ -156,18 +184,41 @@ struct QuickLayoutSpecImp: QuickLayoutSpec {
 
 public enum QuickViewType {
     case plain
+    case label
 }
 
 public protocol QuickViewSpec {
+    var baseSpec: QuickViewBaseSpec { get }
+}
+
+struct QuickViewSpecImp: QuickViewSpec {
+    let baseSpec: QuickViewBaseSpec
+}
+
+public protocol QuickViewBaseSpec {
     var backgroundColor: UIColor? { get }
 
     var cornerRadius: CGFloat? { get }
 }
 
-struct QuickViewSpecImp: QuickViewSpec {
+struct QuickViewBaseSpecImp: QuickViewBaseSpec {
     let backgroundColor: UIColor?
 
     let cornerRadius: CGFloat?
+}
+
+protocol QuickLabelSpec: QuickViewSpec {
+    var text: String? { get }
+    var textColor: UIColor? { get }
+    var font: UIFont? { get }
+}
+
+struct QuickLabelSpecImp: QuickLabelSpec {
+    let baseSpec: QuickViewBaseSpec
+
+    let text: String?
+    let textColor: UIColor?
+    let font: UIFont?
 }
 
 public protocol QuickSpec {
@@ -256,6 +307,17 @@ final class Pinner {
 
         case .marginTop:
             pin.marginTop(const)
+
+        case .after:
+            pin.after(of: <#T##UIView#>, aligned: <#T##VerticalAlign#>)
+        case .before:
+            <#code#>
+        case .sizeToFit:
+            <#code#>
+        case .sizeToFitWidth:
+            <#code#>
+        case .sizeToFitHeight:
+            <#code#>
         }
     }
 
@@ -288,7 +350,24 @@ final class Pinner {
     }
 }
 
-open class QuickView: UIView {
+extension UIView {
+    func setup(with spec: QuickViewSpec) {
+        backgroundColor = spec.baseSpec.backgroundColor
+        if let corner = spec.baseSpec.cornerRadius {
+            layer.cornerRadius = corner
+        }
+    }
+}
+
+protocol IQuickView: class {
+    var identifier: String { get set }
+
+    var spec: QuickSpec { get }
+}
+
+typealias TQuickView = (IQuickView & UIView)
+
+open class QuickLabel: UILabel, IQuickView {
     // MARK: - Members
 
     open var identifier: String
@@ -309,15 +388,61 @@ open class QuickView: UIView {
         self.spec = spec
         super.init(frame: .zero)
 
-        setupView(spec: spec.viewSpec)
+        setupView(with: spec.viewSpec)
         setupSubviews()
     }
 
-    private func setupView(spec: QuickViewSpec) {
-        backgroundColor = spec.backgroundColor
-        if let corner = spec.cornerRadius {
-            layer.cornerRadius = corner
-        }
+    private func setupView(with spec: QuickViewSpec) {
+        setup(with: spec)
+        guard let labelSpec = spec as? QuickLabelSpec else { return }
+
+        text = labelSpec.text
+        textColor = labelSpec.textColor
+        font = labelSpec.font
+    }
+
+    private func setupSubviews() {
+        let producer = Producer()
+        let views = spec.subviews.map(producer.makeView)
+        views.forEach(addSubview)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder _: NSCoder) { fatalError() }
+
+    // MARK: - Layout
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+
+        Pinner.layout(self, specs: spec.layoutSpecs, in: container)
+        subviews.forEach { $0.setNeedsLayout() }
+    }
+}
+
+open class QuickView: UIView, IQuickView {
+    // MARK: - Members
+
+    open var identifier: String
+
+    public let spec: QuickSpec
+
+    private weak var container: UIView?
+
+    // MARK: - Init
+
+    public convenience init(spec: QuickSpec, container: UIView?) {
+        self.init(spec: spec)
+        self.container = container
+    }
+
+    public init(spec: QuickSpec) {
+        identifier = spec.name
+        self.spec = spec
+        super.init(frame: .zero)
+
+        setup(with: spec.viewSpec)
+        setupSubviews()
     }
 
     private func setupSubviews() {
@@ -385,7 +510,7 @@ open class QuickController: UIViewController {
 
     // MARK: - Helpers
 
-    private func makeViews(specs: [QuickSpec]) -> [QuickView] {
+    private func makeViews(specs: [QuickSpec]) -> [TQuickView] {
         let producer = Producer()
 
         return specs.map { producer.makeView(spec: $0, in: view) }
@@ -404,18 +529,18 @@ open class QuickController: UIViewController {
 final class Producer {
     // MARK: - Members
 
-    private typealias Builder = (QuickSpec, UIView?) -> QuickView
+    private typealias Builder = (QuickSpec, UIView?) -> TQuickView
 
     private lazy var bootstrap:
         Bootstrapper = Bootstrapper()
 
     // MARK: - Interface
 
-    func makeView(spec: QuickSpec) -> QuickView {
+    func makeView(spec: QuickSpec) -> TQuickView {
         return makeView(spec: spec, in: nil)
     }
 
-    func makeView(spec: QuickSpec, in container: UIView?) -> QuickView {
+    func makeView(spec: QuickSpec, in container: UIView?) -> TQuickView {
         guard let maker = quickTypeToBuilder[spec.quickType] else {
             assertionFailure()
             return QuickView(spec: spec, container: container)
@@ -426,15 +551,22 @@ final class Producer {
 
     // MARK: - Helpers
 
-    private func makePlainView(spec: QuickSpec, container: UIView?) -> QuickView {
+    private func makePlainView(spec: QuickSpec, container: UIView?) -> TQuickView {
         let plain = QuickView(spec: spec, container: container)
 
         return plain
     }
 
+    private func makeLabel(spec: QuickSpec, container: UIView?) -> TQuickView {
+        let label = QuickLabel(spec: spec, container: container)
+
+        return label
+    }
+
     private lazy var quickTypeToBuilder: [QuickViewType: Builder] = {
         let factory = [
             QuickViewType.plain: makePlainView,
+            QuickViewType.label: makeLabel,
         ]
 
         return factory
@@ -449,8 +581,7 @@ class ViewController: QuickController {
     }
 
     private func testJSON() {
-        let str: String = "{\"name\":\"CardController\",\"type\":0,\"container\":{\"name\":\"container\",\"type\":0,\"subviews\":[{\"name\":\"notification\",\"corner\":\"8\",\"backgroundColor\":\"#343F4B\",\"type\":0,\"subviews\":[{\"name\":\"badge\",\"corner\":\"5\",\"backgroundColor\":\"#C0CCDA\",\"type\":0,\"layout\":[{\"method\":3,\"arguments\":[\"8\"]},{\"method\":7,\"arguments\":[\"8\"]},{\"method\":0,\"arguments\":[\"20\"]}]}],\"layout\":[{\"method\":3,\"arguments\":[\"container.safeArea\"]},{\"method\":9,\"arguments\":[\"16\"]},{\"method\":2,\"arguments\":[\"128\"]},{\"method\":8,\"arguments\":[\"8\"]}]}]}}"
-        if let d = str.data(using: .utf8), let qc = QuickControllerSpecImp(data: d) {
+        if let d = viewJSON.data(using: .utf8), let qc = QuickControllerSpecImp(data: d) {
             setup(with: qc)
         }
     }
@@ -459,16 +590,27 @@ class ViewController: QuickController {
 // ======
 
 let PlainView: Int = 0
+let LabelView: Int = 1
 
 extension QuickViewType {
     init?(rawValue: Int) {
-        switch rawValue {
-        case PlainView:
-            self = .plain
-        default:
-            return nil
-        }
+        guard
+            let value = QuickViewType.IntToType[rawValue]
+        else { return nil }
+
+        self = value
     }
+
+    // MARK: - Strategy
+
+    private static let IntToType: [Int: QuickViewType] = {
+        let strategy: [Int: QuickViewType] = [
+            PlainView: .plain,
+            LabelView: .label,
+        ]
+
+        return strategy
+    }()
 }
 
 // ======
@@ -504,7 +646,10 @@ extension QuickSpecImp {
         self.quickType = quickType
 
         subviews = QuickSpecImp.parseSubviews(json: json)
-        viewSpec = QuickSpecImp.parseViewSpecs(json: json)
+        viewSpec = QuickSpecImp.parseViewSpecs(
+            json: json,
+            viewType: quickType
+        )
         layoutSpecs = QuickSpecImp.parseLayout(json: json)
     }
 
@@ -525,7 +670,10 @@ extension QuickSpecImp {
         self.quickType = quickType
 
         subviews = QuickSpecImp.parseSubviews(json: json)
-        viewSpec = QuickSpecImp.parseViewSpecs(json: json)
+        viewSpec = QuickSpecImp.parseViewSpecs(
+            json: json,
+            viewType: quickType
+        )
         layoutSpecs = QuickSpecImp.parseLayout(json: json)
     }
 
@@ -540,7 +688,20 @@ extension QuickSpecImp {
 
     // MARK: - Helpers
 
-    private static func parseViewSpecs(json: JSON) -> QuickViewSpec {
+    private static func parseViewSpecs(json: JSON, viewType: QuickViewType) -> QuickViewSpec {
+        let baseSpec: QuickViewBaseSpec = parseBaseViewSpecs(json: json)
+
+        switch viewType {
+        case .plain:
+            return parsePlainViewSpecs(base: baseSpec)
+        case .label:
+            return parseLabelViewSpecs(
+                base: baseSpec, json: json
+            )
+        }
+    }
+
+    private static func parseBaseViewSpecs(json: JSON) -> QuickViewBaseSpec {
         let colorValue: String? = json["backgroundColor"] as? String
         var cornerRadius: CGFloat?
 
@@ -549,9 +710,41 @@ extension QuickSpecImp {
             cornerRadius = CGFloat(radiusValue)
         }
 
-        return QuickViewSpecImp(
+        return QuickViewBaseSpecImp(
             backgroundColor: UIColor.hex(colorValue),
             cornerRadius: cornerRadius
+        )
+    }
+
+    private static func parsePlainViewSpecs(base: QuickViewBaseSpec) -> QuickViewSpec {
+        return QuickViewSpecImp(
+            baseSpec: base
+        )
+    }
+
+    private static func parseLabelViewSpecs(base: QuickViewBaseSpec, json: JSON) -> QuickViewSpec {
+        let text = json["text"] as? String
+        let colorValue: String? = json["textColor"] as? String
+        let textColor = UIColor.hex(colorValue)
+
+        var fontSize: CGFloat?
+        let fontDict: JSON? = json["font"] as? JSON
+        let fontName: String? = fontDict?["name"] as? String
+        let fontWeight: Int? = fontDict?["weight"] as? Int
+
+        if let fontSizeProperty = fontDict?["size"] as? String, let fontSizeValue = Double(fontSizeProperty) {
+            fontSize = CGFloat(fontSizeValue)
+        }
+
+        return QuickLabelSpecImp(
+            baseSpec: base,
+            text: text,
+            textColor: textColor,
+            font: UIFont.named(
+                fontName,
+                size: fontSize,
+                weight: fontWeight
+            )
         )
     }
 
@@ -600,3 +793,25 @@ extension QuickSpecImp {
         return nothing
     }
 }
+
+extension UIFont {
+    static func named(_ name: String?, size: CGFloat?, weight: Int?) -> UIFont? {
+        guard let name = name, let size = size, let weight = weight else {
+            return nil
+        }
+        let fontWeight = UIFont.Weight(CGFloat(weight))
+
+        return named(name, size: size, weight: fontWeight)
+    }
+
+    private static func named(_ name: String, size: CGFloat, weight: UIFont.Weight) -> UIFont? {
+        switch name {
+        case "system":
+            return UIFont.systemFont(ofSize: size, weight: weight)
+        default:
+            return nil
+        }
+    }
+}
+
+let viewJSON: String = "{\"name\":\"CardController\",\"type\":0,\"container\":{\"name\":\"container\",\"type\":0,\"subviews\":[{\"name\":\"notification\",\"corner\":\"8\",\"backgroundColor\":\"#343F4B\",\"type\":0,\"subviews\":[{\"name\":\"badge\",\"corner\":\"5\",\"backgroundColor\":\"#C0CCDA\",\"type\":0,\"layout\":[{\"method\":3,\"arguments\":[\"8\"]},{\"method\":7,\"arguments\":[\"8\"]},{\"method\":0,\"arguments\":[\"20\"]}]},{\"name\":\"messageLabel\",\"type\":1,\"layout\":[{\"method\":10,\"arguments\":[\"badge\",\"8\",0]},{\"method\":6,\"arguments\":[\"16\"]},{\"method\":13}],\"text\":\"MESSAGES\",\"textColor\":\"#8392A7\",\"font\":{\"name\":\"system\",\"size\":\"14\",\"weight\":0}}],\"layout\":[{\"method\":3,\"arguments\":[\"container.safeArea\"]},{\"method\":9,\"arguments\":[\"16\"]},{\"method\":2,\"arguments\":[\"128\"]},{\"method\":8,\"arguments\":[\"8\"]}]}]}}"
